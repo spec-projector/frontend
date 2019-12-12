4fe7c0e95ebfb832707a228773b48e8bbde57c0d
Roman Gromov <hanaevroman@gmail.com>
2019-12-12 15:32:47 +0000

    Resolve "Fix bug"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const config = {
     storage: 'http://localhost:5984'
 };
+
+export const environment = {
+    production: true
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,7 @@ export const config = {
     storage: 'https://specprojector.com/couchdb/'
     // storage: 'http://localhost:5984'
 };
+
+export const environment = {
+    production: false
+};
