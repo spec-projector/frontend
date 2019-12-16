574af948b46ae7bd221ee864980dbaed72890cf5
hanaevroman@gmail.com <hanaevroman@gmail.com>
2019-12-16 13:36:33 +0300

    #32

--- src/app/components/spec/terms/terms.component.ts
+++ src/app/components/spec/terms/terms.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UI } from 'junte-ui';
 import { SpecManager } from 'src/app/managers/spec.manager';
 import { EditMode } from 'src/app/model/enums/edit-mode';
-import { UI } from 'junte-ui';
-import { Epic } from "src/app/model/spec/planning/epic";
-import { Term } from "src/app/model/spec/planning/term";
-import { TextToken } from "src/app/model/spec/planning/token";
+import { Term } from 'src/app/model/spec/planning/term';
+import { TextToken } from 'src/app/model/spec/planning/token';
 import { Spec } from 'src/app/model/spec/spec';
 import * as uuid from 'uuid/v1';
+
+class AlphabeticalTerms {
+    char: string;
+    terms: Term[] = [];
+
+    constructor(term: Term) {
+        this.char = term.name[0];
+        this.terms.push(term);
+    }
+}
 
 @Component({
     selector: 'spec-terms',
@@ -18,6 +27,7 @@ export class TermsComponent implements OnInit {
 
     ui = UI;
     editMode = EditMode;
+    terms: AlphabeticalTerms[] = [];
 
     mode = EditMode.view;
     spec: Spec;
@@ -27,7 +37,19 @@ export class TermsComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.route.data.subscribe(({spec}) => this.spec = spec);
+        this.route.data.subscribe(({spec}) => {
+            this.spec = spec;
+            const terms = spec.terms.sort((a, b) => a.name.localeCompare(b.name));
+            terms.forEach(term => {
+                const last = this.terms[this.terms.length - 1];
+
+                if (!!this.terms.length && !!last && last.char === term.name[0]) {
+                    last.terms.push(term);
+                } else {
+                    this.terms.push(new AlphabeticalTerms(term));
+                }
+            });
+        });
     }
 
     addTerm() {
