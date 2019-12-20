57bbf854b4c7042c99b7c9d2627f9de815d18cb1
Anton Breslavsky <breslavsky.anton@gmail.com>
2019-12-20 16:01:06 +0300

    fix epics

--- src/app/components/spec/spec-routing.module.ts
+++ src/app/components/spec/spec-routing.module.ts
@@ -33,7 +33,7 @@ export const routes: Routes = [
             {
                 path: '',
                 pathMatch: 'full',
-                redirectTo: 'sprints'
+                redirectTo: 'details'
             },
             {
                 path: 'details',
