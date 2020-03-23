b8c3e2a70ed9d0d769a481dfc4d0c0d8ef529dbf
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-03-23 17:45:42 +0300

    implement print

--- src/app/components/spec/spec-routing.module.ts
+++ src/app/components/spec/spec-routing.module.ts
@@ -11,96 +11,102 @@ import { SprintsComponent } from 'src/app/components/spec/sprints/sprints.compon
 import { SprintResolver } from 'src/app/components/spec/sprints/sprints.resolver';
 import { TermsComponent } from 'src/app/components/spec/terms/terms.component';
 import { ValidateComponent } from 'src/app/components/spec/validate/validate.component';
+import { PrintComponent } from './print/print.component';
 
 export function getProject(data: any) {
-    return data.project.title;
+  return data.project.title;
 }
 
 export function getSprint(data: any) {
-    return data.sprint.title;
+  return data.sprint.title;
 }
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: SpecComponent,
+    resolve: {
+      spec: SpecResolver,
+      project: ProjectResolver
+    },
+    data: {breadcrumb: getProject},
+    children: [
+      {
         path: '',
-        component: SpecComponent,
-        resolve: {
-            spec: SpecResolver,
-            project: ProjectResolver
-        },
-        data: {breadcrumb: getProject},
+        pathMatch: 'full',
+        redirectTo: 'details'
+      },
+      {
+        path: 'details',
+        component: DetailsComponent,
+        data: {breadcrumb: 'General'}
+      },
+      {
+        path: 'print',
+        component: PrintComponent,
+        data: {breadcrumb: 'Print'}
+      },
+      {
+        path: 'sprints',
+        data: {breadcrumb: 'Sprints'},
         children: [
-            {
-                path: '',
-                pathMatch: 'full',
-                redirectTo: 'details'
-            },
-            {
-                path: 'details',
-                component: DetailsComponent,
-                data: {breadcrumb: 'General'}
-            },
-            {
-                path: 'sprints',
-                data: {breadcrumb: 'Sprints'},
-                children: [
-                    {
-                        path: '',
-                        component: SprintsComponent,
-                        resolve: {spec: SpecResolver},
-                        children: [{
-                            path: ':sprint',
-                            component: SprintComponent,
-                            data: {breadcrumb: getSprint},
-                            resolve: {sprint: SprintResolver}
-                        }],
-                        runGuardsAndResolvers: 'always'
-                    }
-                ]
-            },
-            {
-                path: 'epics',
-                component: EpicsComponent,
-                data: {breadcrumb: 'Epics'},
-                resolve: {spec: SpecResolver}
-
-            },
-            {
-                path: 'actors',
-                component: ActorsComponent,
-                data: {breadcrumb: 'Actors'},
-                resolve: {spec: SpecResolver}
-
-            },
-            {
-                path: 'terms',
-                component: TermsComponent,
-                data: {breadcrumb: 'Knowledge'},
-                resolve: {spec: SpecResolver}
-
-            },
-            {
-                path: 'packages',
-                component: PackagesComponent,
-                data: {breadcrumb: 'Model'},
-                resolve: {spec: SpecResolver}
-
-            },
-            {
-                path: 'validate',
-                component: ValidateComponent,
-                data: {breadcrumb: 'Validate'},
-                resolve: {spec: SpecResolver}
-
-            }
+          {
+            path: '',
+            component: SprintsComponent,
+            resolve: {spec: SpecResolver},
+            children: [{
+              path: ':sprint',
+              component: SprintComponent,
+              data: {breadcrumb: getSprint},
+              resolve: {sprint: SprintResolver}
+            }],
+            runGuardsAndResolvers: 'always'
+          }
         ]
+      },
+      {
+        path: 'epics',
+        component: EpicsComponent,
+        data: {breadcrumb: 'Epics'},
+        resolve: {spec: SpecResolver}
 
-    }
+      },
+      {
+        path: 'actors',
+        component: ActorsComponent,
+        data: {breadcrumb: 'Actors'},
+        resolve: {spec: SpecResolver}
+
+      },
+      {
+        path: 'terms',
+        component: TermsComponent,
+        data: {breadcrumb: 'Knowledge'},
+        resolve: {spec: SpecResolver}
+
+      },
+      {
+        path: 'packages',
+        component: PackagesComponent,
+        data: {breadcrumb: 'Model'},
+        resolve: {spec: SpecResolver}
+
+      },
+      {
+        path: 'validate',
+        component: ValidateComponent,
+        data: {breadcrumb: 'Validate'},
+        resolve: {spec: SpecResolver}
+
+      }
+    ]
+
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class SpecRoutingModule {
 }
