44ce6d261c38d56ebff51cf8f5a1b3808629c2c2
Dmitriy Yashin <chiflash101@gmail.com>
2020-12-30 11:36:05 +0300

    Refactor icons remove fonts icons and refactor enum icons #1807

--- src/enums/local-ui.ts
+++ src/enums/local-ui.ts
@@ -1,44 +1,16 @@
-export enum FontLocalIcons {
-    general = 'general',
-    features = 'features',
-    epics = 'epics',
-    terms = 'terms',
-    sprints = 'sprints',
-    model = 'model',
-    validate = 'validate'
-}
-
-export enum SvgLocalIcons {
-    graphQl = 'graphql',
-    restApi = 'rest',
-    story = 'story',
-    resources = 'resources',
-    issues = 'issues'
-}
-
 export class LocalUI {
     static icons = {
-        font: FontLocalIcons,
-        svg: {
-            default: {
-                graphQl: SvgLocalIcons.graphQl,
-                rest: SvgLocalIcons.restApi,
-                story: SvgLocalIcons.story,
-                resources: SvgLocalIcons.resources,
-                issues: SvgLocalIcons.issues
-            }
-        },
-        sprints: FontLocalIcons.sprints + ':font:icons',
-        terms: FontLocalIcons.terms + ':font:icons',
-        epics: FontLocalIcons.epics + ':font:icons',
-        model: FontLocalIcons.model + ':font:icons',
-        features: FontLocalIcons.features + ':font:icons',
-        validate: FontLocalIcons.validate + ':font:icons',
-        general: FontLocalIcons.general + ':font:icons',
-        graphQl: SvgLocalIcons.graphQl + ':svg:local',
-        rest: SvgLocalIcons.restApi + ':svg:local',
-        story: SvgLocalIcons.story + ':svg:local',
-        resources: SvgLocalIcons.resources + ':svg:local',
-        issues: SvgLocalIcons.issues + ':svg:local'
+        sprints: ':svg:local:stroked',
+        terms: ':svg:local:stroked',
+        epics: ':svg:local:stroked',
+        model: ':svg:local:stroked',
+        features: ':svg:local:stroked',
+        validate: ':svg:local:stroked',
+        general: ':svg:local:stroked',
+        graphQl: ':svg:local',
+        rest: ':svg:local',
+        story: ':svg:local',
+        resources: ':svg:local',
+        issues: ':svg:local'
     };
 }
