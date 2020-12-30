fad331380c755933757c50c1b44edf9eff2cfeef
Anton Breslavsky <breslavsky.anton@gmail.com>
2020-12-30 18:36:54 +0300

    Fix node version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,33 +3,34 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthorizationGuard } from 'src/app/app.guard';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'projects',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        loadChildren: () => import('./login/login.module')
-            .then(m => m.LoginModule)
-    },
-    {
-        path: 'projects',
-        loadChildren: () => import('./layout/layout.module')
-            .then(m => m.LayoutModule),
-        canActivate: [AuthorizationGuard]
-    }
+  {
+    path: '',
+    redirectTo: 'projects',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module')
+      .then(m => m.LoginModule)
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./layout/layout.module')
+      .then(m => m.LayoutModule),
+    canActivate: [AuthorizationGuard]
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {
-        paramsInheritanceStrategy: 'always',
-        relativeLinkResolution: 'corrected',
-        scrollPositionRestoration: 'top',
-        anchorScrolling: 'enabled',
-        scrollOffset: [0, 150]
+  imports: [
+    RouterModule.forRoot(routes, {
+      paramsInheritanceStrategy: 'always',
+      relativeLinkResolution: 'corrected',
+      scrollPositionRestoration: 'top',
+      anchorScrolling: 'enabled',
+      scrollOffset: [0, 150]
     })],
-    exports: [RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
