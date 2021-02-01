d2be9f747236ffe6616918406cf643d33bc97cd4
Anton Breslavsky <breslavsky.anton@gmail.com>
2021-02-01 23:33:10 +0200

    Remove authorization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   uiDebounceTime: 500,
-  storage: 'https://admin:admin@www.specprojector.com/couchdb'
+  storage: 'https://www.specprojector.com/couchdb'
 };
