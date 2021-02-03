2a2574e7ee846be8ac03bddc7ff2293a6b40f11c
Anton Breslavsky <breslavsky.anton@gmail.com>
2021-02-03 13:22:01 +0200

    Return bearer

--- src/managers/spec.manager.ts
+++ src/managers/spec.manager.ts
@@ -67,8 +67,7 @@ export class SpecManager {
           skip_setup: true,
           fetch: (url, opts) => {
             const headers = opts.headers as Headers;
-            // kick publication
-            headers.append('Authorization', this.config.authorization.key);
+            headers.append('Authorization', `Bearer ${this.config.authorization.key}`);
             return PouchDB.fetch(url, opts);
           }
         });
