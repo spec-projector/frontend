985f278d223c9a4785a9a259cb4cb82e7e479c91
Anton Khrypko <joefamwork@gmail.com>
2021-02-12 12:21:44 +0000

    Resolve "Translate to RU"

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from 'src/app/projects/projects.component';
 import { AuthorizationGuard } from '../../guards/authorization';
 
+
+export const PROJECTS_BREADCRUMB = $localize`:@@label.projects:Projects`;
+
 const routes: Routes = [
   {
     path: '',
     component: ProjectsComponent,
-    data: {breadcrumb: 'Projects'},
+    data: {breadcrumb: PROJECTS_BREADCRUMB},
     canActivate: [AuthorizationGuard]
   }
 ];
