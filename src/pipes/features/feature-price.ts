75247f8c1eec288173e56c46bab553c5fa546c43
Anton Breslavsky <breslavsky.anton@gmail.com>
2021-02-15 18:10:18 +0300

    Improve price calculation

--- src/pipes/features/feature-price.ts
+++ src/pipes/features/feature-price.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Feature } from 'src/model/spec/planning/feature';
 
 @Pipe({name: 'featurePrice'})
 export class FeaturePricePipe implements PipeTransform {
-  transform(feature: Feature, ..._): number {
-    return feature.resources.reduce((price, r) => {
-      const resource = feature.spec.resourceTypes.find(res => res.title === r.resource);
-      return price + (!!resource ? resource.hourRate * r.hours : 0);
-    }, 0);
+  transform(feature: Feature, ..._): Observable<number> {
+    return new Observable<number>(o => {
+      setTimeout(() => {
+        const cost = feature.resources.reduce((price, r) => {
+          const resource = feature.spec.resourceTypes.find(res => res.title === r.resource);
+          return price + (!!resource ? resource.hourRate * r.hours : 0);
+        }, 0);
+        o.next(cost);
+        o.complete();
+      }, Math.random() * 500);
+    });
   }
 }
