6cfa704c68994d0b2c7a7e65ad2118102c459021
Anton Breslavsky <breslavsky.anton@gmail.com>
2021-03-02 16:52:17 +0300

    Fix AOT building

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,20 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { JunteUiModule } from '@junte/ui';
+import { JunteUiModule, OtherModule, ShortcutsModule } from '@junte/ui';
 import { LoginRoutingModule } from 'src/app/login/login-routing.module';
 import { LoginComponent } from 'src/app/login/login.component';
 
 @NgModule({
-    declarations: [
-        LoginComponent
-    ],
-    imports: [
-        CommonModule,
-        JunteUiModule,
-        ReactiveFormsModule,
-        LoginRoutingModule
-    ]
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    JunteUiModule,
+    ShortcutsModule,
+    ReactiveFormsModule,
+    LoginRoutingModule
+  ]
 })
 export class LoginModule {
 }
