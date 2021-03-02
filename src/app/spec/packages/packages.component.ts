925bbee74c30d6efafe9db6d9a483d5080b5b24d
breslavsky.anton@gmail.com <229835aA>
2021-03-03 00:20:48 +0300

    Refactor model

--- src/app/spec/packages/packages.component.ts
+++ src/app/spec/packages/packages.component.ts
@@ -1,62 +1,76 @@
-import { moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { SpecManager } from 'src/managers/spec.manager';
-import { EditMode } from 'src/enums/edit-mode';
-import { UI } from '@junte/ui';
-import { Package } from 'src/model/spec/orm/package';
-import { Spec } from 'src/model/spec/spec';
-import * as uuid from 'uuid/v1';
-import { LocalUI } from '../../../enums/local-ui';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {Component, Inject, LOCALE_ID, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {SpecManager} from 'src/managers/spec.manager';
+import {EditMode} from 'src/enums/edit-mode';
+import {PopoverInstance, UI} from '@junte/ui';
+import {Package} from 'src/model/spec/orm/package';
+import {Spec} from 'src/model/spec/spec';
+import {LocalUI} from '../../../enums/local-ui';
+import {Language} from '../../../enums/language';
+import {generate as shortid} from 'shortid';
+import {takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
-    selector: 'spec-packages',
-    templateUrl: './packages.component.html',
-    styleUrls: ['./packages.component.scss']
+  selector: 'spec-packages',
+  templateUrl: './packages.component.html',
+  styleUrls: ['./packages.component.scss']
 })
-export class PackagesComponent implements OnInit {
+export class PackagesComponent implements OnInit, OnDestroy {
 
-    ui = UI;
-    localUi = LocalUI;
-    editMode = EditMode;
+  ui = UI;
+  localUi = LocalUI;
+  editMode = EditMode;
+  language = Language;
 
-    spec: Spec;
+  private destroyed = new Subject();
 
-    constructor(public manager: SpecManager,
-                private route: ActivatedRoute) {
-    }
+  spec: Spec;
+  instance: { popover: PopoverInstance } = {popover: null};
 
-    ngOnInit() {
-        this.route.data.subscribe(({spec}) => this.spec = spec);
-    }
+  constructor(@Inject(LOCALE_ID) public locale: string,
+              public manager: SpecManager,
+              private route: ActivatedRoute) {
+  }
 
-    addPackage() {
-        const pack = new Package({
-            id: uuid(),
-            name: 'package',
-            title: 'Package'
-        });
-        this.spec.packages.unshift(pack);
+  ngOnInit() {
+    this.route.data.pipe(takeUntil(this.destroyed))
+      .subscribe(({spec}) => this.spec = spec);
+  }
 
-        this.manager.put(pack);
-        this.manager.put(this.spec);
-    }
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 
-    deletePackage(index: number) {
-        const pack = this.spec.packages[index];
-        this.spec.packages.splice(index, 1);
+  addPackage() {
+    const pack = new Package({
+      id: shortid(),
+      name: 'package',
+      title: 'Package'
+    });
+    this.spec.packages.unshift(pack);
 
-        this.manager.remove(pack);
-        this.manager.put(this.spec);
+    this.manager.put(pack);
+    this.manager.put(this.spec);
+  }
 
-    }
+  deletePackage(index: number) {
+    const pack = this.spec.packages[index];
+    this.spec.packages.splice(index, 1);
 
-    movePackage(event: any) {
-        moveItemInArray(this.spec.packages, event.previousIndex, event.currentIndex);
-        this.manager.put(this.spec);
-    }
+    this.manager.remove(pack);
+    this.manager.put(this.spec);
 
-    trackPackage(index: number, pack: Package) {
-        return !!pack ? pack.id : null;
-    }
+  }
+
+  movePackage(event: CdkDragDrop<Package[]>) {
+    moveItemInArray(this.spec.packages, event.previousIndex, event.currentIndex);
+    this.manager.put(this.spec);
+  }
+
+  trackPackage(index: number, pack: Package) {
+    return !!pack ? pack.id : null;
+  }
 }
