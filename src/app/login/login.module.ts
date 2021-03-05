f5a6f3c41ac4c0b0482f5dc84d9d06c29cd69bde
Anton Khrypko <joefamwork@gmail.com>
2021-03-05 11:40:23 +0000

    Resolve "Remove unuseful i18n files"

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { JunteUiModule, OtherModule, ShortcutsModule } from '@junte/ui';
+import { JunteUiModule, ShortcutsModule } from '@junte/ui';
 import { LoginRoutingModule } from 'src/app/login/login-routing.module';
 import { LoginComponent } from 'src/app/login/login.component';
 
